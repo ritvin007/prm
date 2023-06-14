fb1f7f6e2fc400aa0aac14d7f5d28704a3b63a32
ritvin007 <ritvin007@gmail.com>
2023-06-14 10:36:37 +0530

    'aws'

--- cmail.py
+++ cmail.py
@@ -2,7 +2,7 @@ import smtplib
 from email.message import EmailMessage
 def sendmail(to,subject,body):
     server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('ritvin007@gmail.com','ralbnispaqulhpkn')
+    server.login('ritvin007@gmail.com','idqovzkoezuqiuqd')
     msg=EmailMessage()
     msg['From']='ritvin007@gmail.com'
     msg['Subject']=subject
